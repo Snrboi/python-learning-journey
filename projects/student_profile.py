99fc9938b91146e501b250196773f029dc551312
Golden <emetogolden@gmail.com>
2026-07-15 15:44:13 +0100

    day-02 week 1

---
+++ projects/student_profile.py
@@ -0,0 +1,18 @@
+print("===================================")
+print("            STUDENT PROFILE")
+print("===================================")
+
+name = "Emeto Golden"
+age = 25    
+state = "Imo State"
+current_program = "Learn2Earn Fellowship"
+dream_career = "AI Software Engineer"
+favorite_football_team = "Chelsea"
+
+
+print("Name: " + name)
+print("Age: " + str(age))
+print("State: " + state)
+print("Current Program: " + current_program)
+print("Dream Career: " + dream_career)
+print("Favorite Football Team: " + favorite_football_team)
